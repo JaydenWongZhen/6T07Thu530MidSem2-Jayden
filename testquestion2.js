fe46d9a280b42a9b030436a033acca5095f00a6a
JaydenWongZhen <taymeister@gmail.com>
2026-09-03 10:04:57 +0000

    Sep 3, 2026, 10:04 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,13 @@
+function setup() {
+    createCanvas(600,400);
+    background(255);
+    world.gravity.y = 5;
+}        
 
+function draw() {
+    background(255);
+    if (mouse.presses()) {
+        ball = new Sprite(mouseX,mouseY,20);
+    
+}
+}
